472627fb24aa6a8483ef69fbd5497b72e7aeaf4f
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-03-15 11:13:03 -0400

    Added bio, education, work, and project JSON objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,76 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var bio = {
+   "name" : "Scott Heffner",
+   "role" : "Network and Systems Administrator",
+   "contacts" : {
+     "mobile" : "(978) 503-2705",
+     "email" : "scott.thomas.heffner@gmail.com",
+     "github" : "sctthffnr",
+     "location" : "Gardner, Massachusetts"
+   },
+   "welcomeMessge" : "Welcome to Scott's Resume",
+   "skills" : [""],
+   "biopic" : "none at the moment"
+ };
+
+ var education = {
+   "schools": [
+     {
+       "name": "Pennsylvania State University",
+       "location": "State College, PA",
+       "degree": "Bachelor of Science",
+       "majors": ["Music Education"],
+       "dates": "1996 - 2000",
+       "url": "www.psu.edu"
+     },
+     {
+       "name": "University of Massachusetts Lowell",
+       "location": "Lowell, MA",
+       "degree": "Bachelor of Science",
+       "majors": ["Information Technology"],
+       "dates": "2009",
+       "url": "www.uml.edu"
+     }
+   ],
+   "onlineCourses": [
+     {
+        "title": "Beginning Ruby",
+        "school": "Udacity",
+        "date": "2016",
+        "url": "www.udacity.com"
+     }
+   ],
+   "display": "placeholder"
+ };
+
+ var work = {
+   jobs: [
+     {
+       "employer": "Town of Northborough",
+       "title": "Technology Specialist",
+       "location": "Northborough, MA",
+       "dates": "2010-2015",
+       "description": "Provided support for end-user devices, networking equipment, servers, and applications"
+     },
+     {
+       "employer": "Town of Carlisle",
+       "title": "Network and Systems Administrator",
+       "location": "Carlisle, MA",
+       "dates": "in progress",
+       "description": "Manage network and systems for the Carlisle School district"
+     }
+   ],
+   "display": "placeholder"
+ };
+
+ var projects = [
+   {
+     "title" : "Survivor Simulation",
+     "dates" : "2016",
+     "description" : "A simulation of the television show survivor, " +
+                     "written in Ruby",
+    "images" : ["https://github.com/sctthffnr/survivor"]
+   }
+ ];
